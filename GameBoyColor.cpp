935f531bb623e5939a907d345c97295262c7a8f5
Kehinde Adeoso <kennolite@gmail.com>
2026-01-20 03:50:31 -0500

    Implemented CPU class constructor

--- GameBoyColor.cpp
+++ GameBoyColor.cpp
@@ -3,3 +3,25 @@
 // Stores the interface for the Game Boy Color CPU core.
 
 
+#include "GameBoyColor.h"
+
+CPU::CPU(const std::string& bootPath, const std::string &cartPath)
+: c_ProgramCounter(0x0000), c_StackPointer(0xFFFE), c_AF(0x1100), c_BC(0x0000), c_DE(0x0000), c_HL(0x0000){
+	// Load BootROM.
+	FILE* file = fopen(bootPath.c_str(),"rb");
+	int b_pos = 0x0000;
+	while (fread(&c_WorkRAM[b_pos], 1, 1, file)) {
+		b_pos++;
+	}
+
+	fclose(file);
+
+	// Load Cartridge.
+	file = fopen(cartPath.c_str(), "rb");
+	int c_pos = 0x0100;
+	while (fread(&c_WorkRAM[c_pos], 1, 1, file)) {
+		c_pos++;
+	}
+	fclose(file);
+	// Set program counter to start at 0x0000, stack pointer at 0xFFFE
+}
