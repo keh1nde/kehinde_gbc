46d3a91cdcd9050e0d196c23436e87c1df6a38cb
Kehinde Adeoso <kennolite@gmail.com>
2026-04-22 16:52:35 -0400

    Implemented GBC_CART class for testing.

--- GBC_CART.cpp
+++ GBC_CART.cpp
@@ -4,8 +4,48 @@
 
 #include "GBC_CART.h"
 
-#include "GBC_BUS.h"
 
-BYTE GBC_CART::read_rom(WORD addr) const {
+GBC_CART::GBC_CART(const std::string &romPath) {
+	if (!romPath.empty()) {
+		FILE* file = fopen(romPath.c_str(), "rb");
+
+		if (!file) {
+			throw std::runtime_error("Could not open cartridge file");
+		}
+
+		fseek(file, 0, SEEK_END);
+
+		const long size = ftell(file);
+		// std::cout << size << std::endl;
+
+		fseek(file, 0, SEEK_SET);
+		if (size > c_CartridgeROM.size()) throw std::runtime_error("ROM size exceeds max size.");
+
+		// fread(c_CartridgeROM.data(), 1, size, file);
+
+		const size_t bytes_read = fread(c_CartridgeROM.data(), 1, size, file);
+		std::cerr << "bytes_read=" << bytes_read << "\n";
+		std::cerr << "byte @ 0x20D = 0x" << std::hex << static_cast<int>(c_CartridgeROM[0x20D]) << "\n";
+		std::cerr << "byte @ 0x7000 = 0x" << std::hex << static_cast<int>(c_CartridgeROM[0x7000]) << "\n";
+
+		fclose(file);
+	}
+}
+
+BYTE GBC_CART::read_rom(const WORD addr) const {
 	return c_CartridgeROM[addr];
 }
+
+void GBC_CART::write_rom(WORD addr, BYTE value) {
+
+}
+
+
+BYTE GBC_CART::read_ram(const WORD addr) {
+	return addr;
+}
+
+void GBC_CART::write_ram(WORD addr, BYTE input) {
+
+}
+
