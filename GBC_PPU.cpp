59bd3aa84186d05a4e8888ed103ea4344b02ffd5
Kehinde Adeoso <kennolite@gmail.com>
2026-04-22 16:53:39 -0400

    Implemented members for testing.

--- GBC_PPU.cpp
+++ GBC_PPU.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "GBC_PPU.h"
+
+BYTE GBC_PPU::read(const WORD addr) {
+	if (addr < 0xA000 && addr >= 0x8000) return ppu_VRAM[0][addr-0x8000];
+	if (addr < 0xFEA0 && addr >= 0xFE00) return ppu_OAM[addr-0xFE00];
+}
+
+void GBC_PPU::write(const WORD addr, const BYTE val) {
+	if (addr < 0xA000 && addr >= 0x8000) {
+		ppu_VRAM[0][addr-0x8000] = val;
+	}
+
+	if (addr < 0xFEA0 && addr >= 0xFE00) {
+		ppu_OAM[addr-0xFE00] = val;
+	}
+}
