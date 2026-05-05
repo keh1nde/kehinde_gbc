e98cf6bbb4adc47ea96e345ed7ad2dc7a54500b6
Kehinde Adeoso <kennolite@gmail.com>
2026-05-05 01:34:54 -0400

    Completed Timer implementation

--- GBC_TIMER.h
+++ GBC_TIMER.h
@@ -4,10 +4,30 @@
 
 #ifndef KEHINDES_GAMEBOYCOLOR_EMULATOR_GBC_TIMER_H
 #define KEHINDES_GAMEBOYCOLOR_EMULATOR_GBC_TIMER_H
-
+#include "GBC_Types.h"
 
 
 class GBC_TIMER {
+public:
+	GBC_TIMER() = default;
+	void resetPostBoot();
+	void tick(BYTE cycles);
+	BYTE read(WORD addr) const;
+	void write(WORD addr, BYTE val);
+
+	void set_bus(IBus* bus) { bus_ = bus; }
+private:
+	// Data members
+	IBus* bus_ = nullptr;
+	WORD div_counter_;
+	WORD tima_sub_;
+	BYTE tima_;
+	BYTE tma_;
+	BYTE tac_;
+	// BYTE reload_delay_pending_; If I have time.
+
+	// Helper functions
+	WORD tima_period_() const;
 
 };
 
