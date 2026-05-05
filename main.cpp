6af73fbf625b1292b06f93c058e3aab14f543837
Kehinde Adeoso <kennolite@gmail.com>
2026-05-05 01:35:24 -0400

    Added new function calls to main()

--- main.cpp
+++ main.cpp
@@ -2,14 +2,8 @@
 #include "GBC_CPU.h"
 #include "GBC_PPU.h"
 #include "GBC_CART.h"
-#include "TestBus.h"
-#include "TestCart.h"
-
-/*
- * Hypotheses:
- * 1. The read and write methods aren't declared in CPU, but in Bus. The CPU uses IBus but the
- *
- */
+#include "GBC_TIMER.h"
+#include "GBC_DISPLAY.h"
 
 int main(int argc, char* argv[]) {
 
@@ -17,18 +11,42 @@ int main(int argc, char* argv[]) {
 
 	GBC_PPU ppu{};
 	GBC_CART cart(romPath);
-	GBC_BUS bus("", cart, ppu);
+	std::cerr << "header[0x143]=0x" << std::hex << static_cast<int>(cart.read_rom(0x0143)) <<
+ "\n";
 
+	GBC_TIMER timer{};
+	GBC_BUS bus("", cart, ppu, timer);
 	GBC_CPU cpu(bus);
 
-
+	ppu.set_bus(&bus);
+	timer.set_bus(&bus);
 	cpu.resetPostBoot();
-	for (int i = 0; i < 50000000; ++i) {
+	ppu.resetPostBoot();
+	timer.resetPostBoot();
+
+	GBC_DISPLAY display;
+	if (!display.init(4)) return 1;
+
+	bool running = true;
+	while (running) {
 		try {
-			cpu.execute();
+			const int cycles = cpu.execute();
+			ppu.tick(cycles);
+			timer.tick(cycles);
+			const int irq_cycles = cpu.serviceInterrupts();
+			if (irq_cycles) {
+				ppu.tick(irq_cycles);
+				timer.tick(irq_cycles);
+			}
+
+			if (ppu.consume_frame_ready()) {
+				display.present(ppu.framebuffer_view());
+				running = display.pump();
+			}
 		} catch (const std::exception& e) {
-			std::cerr << "[i=" << i << "] "<< "Caught the following error:" << e.what() << std::endl;
+			std::cerr << "Caught the following error: " << e.what() << std::endl;
 			break;
 		}
 	}
+	return 0;
 }
