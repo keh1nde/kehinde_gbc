2b2b45504d2706a7186a482d582bad39322d560c
Kehinde Adeoso <kennolite@gmail.com>
2026-09-05 14:05:13 -0400

    test: make GoogleTest collision fixes
    - add `DISCOVER_MODE PRE_TEST` keyword argument to CMake command to prevent GoogleTest from being compiled/linked in cases where the tests are not being preformed.
    - implement a null guard to get_utils, preventing the filesystem from attempting to access a path that doesn't exist.

--- test-sys/test_utils.cpp
+++ test-sys/test_utils.cpp
@@ -7,6 +7,7 @@
 
 std::vector<fs::path> get_utils(const fs::path& path, const std::string& extension) {
 	std::vector<fs::path> utils;
+	if (!fs::exists(path)) return utils;
 	for (const auto& entry : fs::recursive_directory_iterator(path)) {
 		if (entry.is_regular_file() && entry.path().extension() == extension) utils.push_back(entry.path());
 	}
